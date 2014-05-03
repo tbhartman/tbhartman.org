782779f8c892921939bfc028901c1f1595c3c826
T.B. Hartman <tbhartman@gmail.com>
2014-05-03 18:18:03 -0400

    Add honda photos

---
+++ for_sale/1998_honda_accord/hover.js
@@ -0,0 +1,8 @@
+function show() {
+    document.getElementById('hidden').style.visibility = 'visible';
+    document.getElementById('img').style.opacity = (0.4);
+}
+function hide() {
+    document.getElementById('hidden').style.visibility = 'hidden';
+    document.getElementById('img').style.opacity = (1.0);
+}
