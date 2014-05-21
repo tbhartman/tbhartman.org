f520971928bceac7a156c1d633a8c9929a21522b
T.B. Hartman <tbhartman@gmail.com>
2014-05-21 14:28:33 -0400

    remove 1998 honda info (sold)

--- for_sale/1998_honda_accord/hover.js
+++
@@ -1,8 +0,0 @@
-function show() {
-    document.getElementById('hidden').style.visibility = 'visible';
-    document.getElementById('img').style.opacity = (0.4);
-}
-function hide() {
-    document.getElementById('hidden').style.visibility = 'hidden';
-    document.getElementById('img').style.opacity = (1.0);
-}
